0dbe8a6505c6526875060b140abfb78f7e5788d2
hasan zargar <hassanzargar123@gmail.com>
2022-04-28 03:27:09 +0430

    adding findnumber(2)

---
+++ findnumber2.py
@@ -0,0 +1,21 @@
+#Cto Profile : Hasan Meqdadi
+
+
+
+
+number = int(input("Hello,wellcom to pythone class.\n you shud find the trusted number.\n pleas enter your number:"))
+if number==2:
+    print("Congratulations, you have found the requested number")
+
+counter = 0
+while number !=2:
+
+    counter += number
+    number = int(input("Please try another number (:"))
+    print(counter)
+    if number==2:
+        print("Congratulations, you have found the requested number")
+
+
+        # //stopship : 4/26/2022 AD  
+
