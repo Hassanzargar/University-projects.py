3a2d08f989649c92333bee5dbeeaf6abdce519fc
hasan zargar <hassanzargar123@gmail.com>
2022-04-28 06:00:55 +0430

    magsoomaleh

---
+++ magsoomaleh.py
@@ -0,0 +1,12 @@
+#Cto Profile : Hasan Meqdadi
+
+def divisor(n):
+    for i in range(1, number):
+     if number % i == 0:
+        print(i)
+
+number = int(input('Enter your number: '))
+divisor(number)
+print(number)
+
+        # //stopship : 4/26/2022 AD  
